30578b762e18d688467f1ab145da3b35f18a7b49
Irving Zamora <irvingzamora28@gmail.com>
2023-02-21 11:05:44 -0600

    Install react-icons

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { AiOutlineUser, AiOutlineUserDelete } from 'react-icons/ai';
 
 function App() {
     return (
         <div className="App">
             <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>
+            <AiOutlineUser />
+            <AiOutlineUserDelete />
         </div>
     );
 }
