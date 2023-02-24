236b4e8a20c537ee6ff8d7138a57bafee7067ac1
Irving Zamora <irvingzamora28@gmail.com>
2023-02-23 20:04:02 -0600

    Add react-router-dom Add Home and Login view components

---
+++ react/src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./views/Home";
+import Login from "./views/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+]);
+
+export default router
