97a4a6388411f2d1b925eb226cf240ab23175ea0
Irving Zamora <irvingzamora06@gmail.com>
2023-03-13 23:04:26 -0600

    Use contextProvider in DefaultLayout

--- react/src/components/DefaultLayout.tsx
+++ react/src/components/DefaultLayout.tsx
@@ -2,13 +2,10 @@ import { Fragment, MouseEvent } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import { NavLink, Outlet } from 'react-router-dom'
+import { userStateContext } from '../contexts/ContextProvider'
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
+
+
 const navigation = [
   { name: 'Dashboard', to: '/', current: true },
   { name: 'Surveys', to: '/surveys', current: false },
@@ -19,6 +16,8 @@ function classNames(...classes: string[]) {
 }
 
 const DefaultLayout: React.FC<{}> = () => {
+
+    const { currentUser, setCurrentUser } = userStateContext();
 
     const logout = (event: MouseEvent) => {
         event.preventDefault()
@@ -82,7 +81,7 @@ const DefaultLayout: React.FC<{}> = () => {
                         <div>
                           <Menu.Button className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="sr-only">Open user menu</span>
-                            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+                            <img className="h-8 w-8 rounded-full" src={currentUser.imageUrl} alt="" />
                           </Menu.Button>
                         </div>
                         <Transition
@@ -141,11 +140,11 @@ const DefaultLayout: React.FC<{}> = () => {
                 <div className="border-t border-gray-700 pt-4 pb-3">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                      <img className="h-10 w-10 rounded-full" src={currentUser.imageUrl} alt="" />
                     </div>
                     <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-white">{user.name}</div>
-                      <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
+                      <div className="text-base font-medium leading-none text-white">{currentUser.name}</div>
+                      <div className="text-sm font-medium leading-none text-gray-400">{currentUser.email}</div>
                     </div>
                     <button
                       type="button"
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom'
+import { ContextProvider } from './contexts/ContextProvider'
 import './index.css'
 import router from './router'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ContextProvider>
+        <RouterProvider router={router} />
+    </ContextProvider>
   </React.StrictMode>
 )
