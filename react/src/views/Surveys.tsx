d3786491a738fb044137b9890b79f1515630e75b
Irving Zamora <irvingzamora28@gmail.com>
2023-03-16 16:49:25 -0600

    Add temporary survey data to ContextProvider

--- react/src/views/Surveys.tsx
+++ react/src/views/Surveys.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Page from "../components/Page";
+import { userStateContext } from "../contexts/ContextProvider";
 
 const Surveys: React.FC<{}> = () => {
+    const { surveys } = userStateContext();
+
     return (
         <>
             <Page title="Survey">
